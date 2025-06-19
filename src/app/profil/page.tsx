b4cf75fa7f5cdc48123c38e9cc4b0a57bc26a50d
alvian-dave <alviandave69@gmail.com>
2025-06-19 11:26:55 +0700

    add

--- src/app/profil/page.tsx
+++ src/app/profil/page.tsx
@@ -1,4 +1,4 @@
-import { auth } from '@/lib/auth';
+import { auth } from '@/auth';
 import { Page } from '@/components/PageLayout';
 import { UserInfo } from '@/components/UserInfo';
 import { ViewPermissions } from '@/components/ViewPermissions';
