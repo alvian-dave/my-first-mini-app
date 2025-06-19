6ba4cee722b8f8455a56764f1289f89ff0130731
alvian-dave <alviandave69@gmail.com>
2025-06-19 23:22:41 +0700

    add

--- src/app/(protected)/home/page.tsx
+++ src/app/(protected)/home/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
   title=""
 />
 </Page.Header>
-      <Page.Main className="flex flex-col items-center justify-center min-h-[calc(100vh-150px)]">
+      <Page.Main className="flex flex-col items-center justify-center min-h-[calc(100vh-150px)] -mt-8">
   <Amount />
 </Page.Main>
     </>
