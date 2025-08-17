6ea14d0403c36e22bc99121db8f68aeb477d5166
Alvian-dave <alviandave69@gmail.com>
2025-08-17 07:00:58 +0700

    dsf

--- src/components/PageLayout/index.tsx
+++ src/components/PageLayout/index.tsx
@@ -34,6 +34,7 @@ const Main = (props: { children: React.ReactNode; className?: string }) => {
     <main
       className={twMerge(
         clsx(
+          'relative grow overflow-y-auto p-6 pt-3',
           'bg-blue-200',
           props.className
         )
