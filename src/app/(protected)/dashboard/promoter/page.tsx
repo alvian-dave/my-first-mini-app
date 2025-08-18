374042b53f27975a2a540b98cc3766b3287f491b
Alvian-dave <alviandave69@gmail.com>
2025-08-18 11:45:59 +0700

    top

--- src/app/(protected)/dashboard/promoter/page.tsx
+++ src/app/(protected)/dashboard/promoter/page.tsx
@@ -3,7 +3,7 @@
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
-import Topbar from '@/components/Topbar'
+import { Topbar } from '@/components/Topbar'
 import GlobalChatRoom from '@/components/GlobalChatRoom'
 import CampaignForm from '@/components/CampaignForm'
 import CampaignTabs from '@/components/CampaignTabs'
