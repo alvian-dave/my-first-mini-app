1866454a9ccebb9ac984418789535ad6df4f8f5a
Alvian-dave <alviandave69@gmail.com>
2025-08-18 08:45:38 +0700

    sdf

--- src/app/(protected)/home/page.tsx
+++ src/app/(protected)/home/page.tsx
@@ -4,7 +4,7 @@ import { Login } from '@/components/Login';
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-900 to-gray-800 text-white px-4">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-900 to-gray-800 px-4">
       <Login />
     </div>
   );
