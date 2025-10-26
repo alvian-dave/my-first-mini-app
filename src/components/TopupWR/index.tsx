b1d94b198996bca6d12d7725c6666411590ceb9a
Alvian-dave <alviandave69@gmail.com>
2025-10-26 19:04:53 +0700

    abi permit dsf

--- src/components/TopupWR/index.tsx
+++ src/components/TopupWR/index.tsx
@@ -64,23 +64,30 @@ export default function TopupWR({ onClose }: TopupWRProps) {
       const deadline = Math.floor((Date.now() + 60 * 60 * 1000) / 1000).toString() // 1 jam dari sekarang
 
       const permitArg = {
-        token: USDC_ADDRESS,
-        amount: usdcAmount,
+        permitted: { token: USDC_ADDRESS, amount: usdcAmount },
+        spender: CONTRACT_ADDRESS,
         nonce,
-       deadline,
-       signature: 'PERMIT2_SIGNATURE_PLACEHOLDER_0',
+        deadline,
       }
-      
+
       const txResult = await MiniKit.commandsAsync.sendTransaction({
         transaction: [
           {
             address: CONTRACT_ADDRESS,
             abi,
             functionName: 'topupWithUSDCWithPermit2',
-            args: [permitArg], // MiniKit auto generate signature
+            args: [permitArg, 'PERMIT2_SIGNATURE_PLACEHOLDER_0'], // MiniKit auto generate signature
           },
         ],
-        permit2: [permitArg], // MiniKit pakai untuk generate signature
+        permit2: [
+          {
+          token: permitArg.permitted.token,
+          amount: permitArg.permitted.amount,
+          nonce: permitArg.nonce,
+          deadline: permitArg.deadline,
+          signature: 'PERMIT2_SIGNATURE_PLACEHOLDER_0',
+          }
+          ]
       })
 
       const successPayload = txResult.finalPayload as MiniAppSendTransactionSuccessPayload
